da329a15099a8daa3078b11c0eb85646054dca48
13691579846 <281754043@qq.com>
2019-07-07 16:42:28 +0800

    modify requests code

--- common/SendRequests.py
+++ common/SendRequests.py
@@ -17,42 +17,35 @@ class HttpRequests(object):
     def __init__(self):
         self.session = requests.Session()
         log.info('建立请求...')
-    def send_request(self, method, url,
-                     params_type='form', data=None,
-                     headers=None, files=None, **kwargs):
+
+    def send_request(self, method, url, params_type='form', data=None, **kwargs):
         method = method.upper()
         params_type = params_type.upper()
         if isinstance(data, str):
             try:
                 data = json.loads(data)
             except Exception:
-                try:
                     data = eval(data)
-                except Exception as e:
-                    raise e
         if 'GET' == method:
-            response = self.session.request(method=method, url=url, params=data, headers=headers, **kwargs)
+            response = self.session.request(method=method, url=url, params=data, **kwargs)
         elif 'POST' == method:
             if params_type == 'FORM':
                 log.info("开始发送{}请求，URL为：{}，请求数据为:{}".format(method, url, data))
-                response = self.session.request(method=method, url=url, data=data, headers=headers,
-                                                files=files, **kwargs)
+                response = self.session.request(method=method, url=url, data=data, **kwargs)
             elif params_type == 'JSON':
-                response = self.session.request(method=method, url=url, json=data, headers=headers,
-                                                files=files, **kwargs)
+                response = self.session.request(method=method, url=url, json=data, **kwargs)
             else:
-                log.error("请求数据格式错误：request params type '{}' error ! please check".format(params_type))
-                raise ValueError('request params type "{}" error ! please check'.format(params_type))
+                response = self.session.request(method=method, url=url, **kwargs)
         else:
             log.error("请求方法错误：request method '{}' error ! please check".format(method))
             raise ValueError('request method "{}" error ! please check'.format(method))
         return response
 
-    def __call__(self, method, url, params_type='form', data=None, headers=None, cookies=None, files=None, **kwargs):
+    def __call__(self, method, url, params_type='form', data=None, **kwargs):
         return self.send_request(method, url,
                                  params_type=params_type,
                                  data=data,
-                                 headers=headers, files=files, **kwargs)
+                                 **kwargs)
 
     def close_session(self):
         self.session.close()
